ef341bf23cddf7a418b93fa31b16d0896d86f427
Brian Henry <94brianhenry@gmail.com>
2018-04-06 09:50:48 -0700

    Add initial js with some functions

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// Back End
+var pizzaSize = "#size";
+var theToppings = "#toppings";
+var sizeArray = [];
+var topArray = [];
+
+//Front End
+$(document).ready(function() {
+  $("form#pizzaSize").forEach(function(event) {
+    var diceRoll = .val()
+    sizeArray.push(initial)
+
+$(document).ready(function() {
+  $("form#theToppings").forEach(function(event) {
+    var diceRoll = .val()
+    sizeArray.push(initial)
