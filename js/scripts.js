b6b1999f1ff89894b5d8cffa72897881b3a9744a
Brian Henry <94brianhenry@gmail.com>
2018-04-06 10:24:57 -0700

    Add loop for items on checkbox

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,29 @@ var pizzaSize = "##size";
 var theToppings = "#toppings";
 var sizeArray = [];
 var topArray = [];
+var pizzaArray = [];
 
 //Front End
 $(document).ready(function() {
   $("form#pizzaSize").forEach(function(event) {
+  checkboxObject.checked = true{
     var pizzaSize = document.getElementById("#size").checked;
-    sizeArray.push(initial)
+    for (i = 0; i < pizzaSize.length; i++) {
+  if (item1[i].checked) {
+    txt = txt + item1[i].value + " ";
+  }
+}
+document.getElementById("order").value = "You ordered a coffee with: " + txt;
+    sizeArray.push[sizeArray]
 
 $(document).ready(function() {
   $("form#theToppings").forEach(function(event) {
+    checkboxObject.checked = true|false
     var theToppings = document.getElementById("#toppings").checked;
-    topArray.push(initial)
+    for (i = 0; i < theToppings.length; i++) {
+  if (item1[i].checked) {
+    txt = txt + item1[i].value + " ";
+  }
+}
+document.getElementById("orderForm").value = "You ordered a pizza with: " + txt;
+    topArray.push[pizzaArray]
